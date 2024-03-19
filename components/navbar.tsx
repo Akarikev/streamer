8f78205238538ab2855defc505bd48150717e1ca
Akarikev <princeelorm17@gmail.com>
2024-03-19 07:21:49 +0000

    mod: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,40 @@
+import { buttonVariants } from "./ui/button";
+
 import React from "react";
 import { ModeToggle } from "@/components/moodtoggler";
+import Link from "next/link";
 function Navbar() {
   return (
     <div>
-      <div className="border-b h-14 border-b-gray-600  mt-2 flex justify-between px-3 ">
+      <div className="border-b h-14 border-b-gray-600   flex justify-between px-6 items-center md:px-10">
+        <div className="flex justify-between items-center gap-3">
+          <h1 className="text-bold text-xl text-green-300">Streamer</h1>
+
+          <Link
+            href={""}
+            className={buttonVariants({
+              variant: "link",
+            })}
+          >
+            link 1
+          </Link>
+          <Link
+            href={""}
+            className={buttonVariants({
+              variant: "link",
+            })}
+          >
+            link 2
+          </Link>
+          <Link
+            href={""}
+            className={buttonVariants({
+              variant: "link",
+            })}
+          >
+            link 3
+          </Link>
+        </div>
         <ModeToggle />
       </div>
     </div>
